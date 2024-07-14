4074babdf2911d3a21d61bb46b8e375fed726f3d
Patrick Oguaju <Patrick_Oguaju@next.co.uk>
2024-07-14 22:04:18 +0100

    implement custom response

---
+++ eTicketManagement.Application/Responses/BaseResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTicketManagement.Application.Responses
+{
+    public class BaseResponse
+    {
+        public BaseResponse() 
+        {
+            Success = true;
+        }
+        public BaseResponse(string message)
+        {
+            Success = true;
+            Message = message;
+        }
+
+        public BaseResponse(string message, bool success)
+        {
+            Success = success;
+            Message = message;
+        }
+
+
+
+        public bool Success { get; internal set; }
+        public string Message { get; set; } =string.Empty;
+        public List<string> ValidationErrors { get; internal set; }
+    }
+}
