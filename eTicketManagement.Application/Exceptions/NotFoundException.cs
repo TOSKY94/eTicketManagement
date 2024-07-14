7bd5a65e8be7dc6e9666a22682b2d18e99e61c62
Patrick Oguaju <Patrick_Oguaju@next.co.uk>
2024-07-14 21:38:14 +0100

    created custom exceptions

---
+++ eTicketManagement.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTicketManagement.Application.Exceptions
+{
+    public class NotFoundException: Exception
+    {
+        public NotFoundException(string name, object key): base($"{name} ({key}) is not found") { }
+    }
+}
---
+++ eTicketManagement.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,20 @@
+﻿
+
+using FluentValidation.Results;
+
+namespace eTicketManagement.Application.Exceptions
+{
+    public class ValidationException:Exception
+    {
+        public List<string> ValidationErrors {  get; set; }
+        public ValidationException(ValidationResult validationResult) 
+        {
+            ValidationErrors = new List<string>();
+
+            foreach(var error in validationResult.Errors)
+            {
+                ValidationErrors.Add(error.ErrorMessage);
+            }
+        }
+    }
+}
